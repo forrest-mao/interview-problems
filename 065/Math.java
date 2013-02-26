d3724872097d5454628405325698ea817d2516d8
wangjinlei <wangjinlei4ustc@gmail.com>
2013-02-26 10:47:54 +0800

    update comment

--- 065/Math.java
+++ 065/Math.java
@@ -5,9 +5,20 @@
 
 public class Math {
 
-	// exponent negtive ? 
+	public static int abs(int d) {
+		if (d < 0) {
+			return 0 - d;
+		}
+		return d;
+	}
+
+	// poor implementation
 	public static double poorPower(double base, int exponent) {
-		
+		boolean negative = (exponent < 0);
+		return negative ? (1 / poorHelper(base, abs(exponent))) : poorHelper(base, abs(exponent));
+	}
+	
+	private static double poorHelper(double base, int exponent) {
 		double result = 1.0d;
 		for (int i = 0; i < exponent; i++) {
 			result = result * base;
@@ -16,12 +27,21 @@ public class Math {
 	}
 	
 	
+	// We will provide an elegant implementation for you.
+	// b^8 = ((b^2)^2)^2  b^20 = b^16 * b^4 = ...
+	private static double elegantHelper(double base, int exponent) {
+		double result = 1.0d;
+		
+		
+		return result;
+	}
+	
 	public static double power(double base, int exponent) {
 
 		return 0.0d;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(1.21 * 1.21);
+		System.out.println(poorPower(2.0, -3));
 	}
 }

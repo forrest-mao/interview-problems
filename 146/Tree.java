7034dd68c11a0ae8858837fc8affb06984283651
wangjinlei <wangjinlei4ustc@gmail.com>
2013-03-12 15:51:49 +0800

    tree add

---
+++ 146/Tree.java
@@ -0,0 +1,26 @@
+
+class Node {
+	int value;
+	Node lchild;
+	Node rchild;
+}
+
+public class Tree {
+
+	public Node root;
+
+	public Tree() {
+	}
+
+	public Node insert(Node n) {
+
+		return root;
+	}
+
+	private Node insert(Node root, Node n) {
+
+	}
+
+	public void print() {
+	}
+}

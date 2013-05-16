63ea45082dcd01267bb316b55344df85427d29ff
wangjinlei <wangjinlei4ustc@gmail.com>
2013-05-16 16:26:03 +0800

    update 002

--- 002/Atoi.java
+++ 002/Atoi.java
@@ -5,22 +5,43 @@
  
 public class Atoi {
 	
-	public static long atoi(String s) {
+	// alpabet to long
+	public static int atoi(String s) {
 		
 		return helper(0, s);
 	}
 	
-	private static long helper(int index, String s) {
+	private static int helper(int index, String s) {
 		boolean negative = false;
 		char firstChar = s.charAt(0);
 		if (firstChar == '-') {
-			negtive = true;
+			negative = true;
 			++index;
 		}
-		return 
+		return -1;
 	}
+	 
+	 
+	public static long atol(String s) {
+		int sign = 1;
+		long result = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (i == 0 && s.charAt(0) == '-') {
+				sign = -1;
+			} else if (s.charAt(i) > '0' && s.charAt(i) < '9') {
+				result = 10 * result + (s.charAt(i) - '0');
+			} else {
+				throw new RuntimeException("Invalid input!");
+			}
+		}
+		
+		return sign * result;
+	} 
 	 
 	public static void main(String[] args) {
 		// to be filled here....		
+
+		System.out.println(atol("-123"));
+		System.out.println(atol("321232"));
 	}
 }
